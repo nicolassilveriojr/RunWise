237e3c0dfa411890db021cce1f150cd62bbd773e
Nicolas <nicolas.silverio.rodrigues@escola.pr.gov.br>
2026-03-28 01:48:25 -0300

    feat: Adicionando api e arrumando outras partes do code

---
+++ index.js
@@ -0,0 +1,12 @@
+﻿const express = require('express');
+const cors = require('cors'); // <--- Importante
+const app = express();
+
+app.use(cors()); // <--- Isso libera o acesso para o Frontend
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ mensagem: 'API e Frontend conectados!' });
+});
+
+app.listen(3000, () => console.log('Servidor ON'));
